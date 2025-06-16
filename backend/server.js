879f76157e1055ac288201e4ffc568dc6317f931
jayeshpatil25 <jayeshpatil7170@gmail.com>
2025-06-16 18:48:29 +0530

    just added nodemailer dependency

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log("MongoDB is connected"))
+.then(() => console.log("MongoDB is successfully connected"))
 .catch(err => console.log(err));
 
 app.use('/api/auth', authRoutes);

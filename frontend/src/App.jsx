af02d3a2a34f612b79901cc57a96aa8189ffa680
Aarya Agrawal <aarya6105@gmail.com>
2025-06-17 12:40:44 +0530

    added the routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,22 @@
 import Login from './pages/Login'
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Dashboard from './pages/Dashboard'
+import './App.css'
 
 function App() {
 
   return (
-    <>
-      <Login />
-    </>
+  
+   <div className="App">
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+   </div>
+      
+   
   )
 }
 

6bd52ec5d4ab65e7731616efeae60ebcff70a088
jayeshpatil25 <jayeshpatil7170@gmail.com>
2025-06-17 12:54:34 +0530

    Login and Signup page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,8 @@
 import Login from './pages/Login'
+import Signup from './pages/Signup'
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import Dashboard from './pages/Dashboard'
+// import Dashboard from './pages/Dashboard'
 import './App.css'
 
 function App() {
@@ -10,7 +11,7 @@ function App() {
   
    <div className="App">
       <Routes>
-        <Route path="/" element={<Dashboard />} />
+        {/* <Route path="/" element={<Dashboard />} /> */}
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>

2fb3988b99b57d3d867a2067d19cde1f719ad796
Aarya Agrawal <aarya6105@gmail.com>
2025-06-17 15:27:00 +0530

    Made some changes to signup

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,13 +1,6 @@
-import axios from 'axios';
+import axios from 'axios'
 
-const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
-});
-
-API.interceptors.request.use((req) => {
-  const token = localStorage.getItem('token');
-  if (token) req.headers.Authorization = `Bearer ${token}`;
-  return req;
-});
-
-export default API;
+export const axiosInstance = axios.create({
+    baseURL:"http://localhost:5000/api",
+    withCredentials :true
+})

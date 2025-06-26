c87b711269f5cda7f266a96a83bf95aaf530fcf3
jayeshpatil25 <jayeshpatil7170@gmail.com>
2025-06-26 23:07:48 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const axiosInstance = axios.create({
-    baseURL:"http://localhost:5000/api",
+    baseURL:"https://triphub-backend-3x8e.onrender.com/api",
     withCredentials :true
 });
 
